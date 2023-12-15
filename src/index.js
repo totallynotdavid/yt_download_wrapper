1e7e2449b2a6767d5048690eee91e3964d44885e
David Duran <dadch1404@gmail.com>
2023-12-15 00:40:48 -0500

    Add support for specifying download quality

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const config = require("../config/production.config")
  * @param {number} [startTime] - The start time for video trimming (in seconds).
  * @param {number} [endTime] - The end time for video trimming (in seconds).
  * @param {string} [format] - The desired output video format.
+ * @param {string} [quality] - The desired quality for downloading (e.g., 'best', 'worst').
  * @returns {Promise<string>} - The path to the processed video file.
  */
 async function processYouTubeVideo(
@@ -17,11 +18,18 @@ async function processYouTubeVideo(
   startTime,
   endTime,
   format = config.defaultFormat,
+  quality = config.defaultQuality,
 ) {
   try {
     ensureDirectoryExists(path.resolve(config.mediaAssetsFolder))
 
-    let downloadedFilePath = await downloadVideo(youtubeId, startTime, endTime)
+    let downloadedFilePath = await downloadVideo(
+      youtubeId,
+      startTime,
+      endTime,
+      format,
+      quality,
+    )
     if (!downloadedFilePath) {
       throw new Error("Failed to download the video or parse the file name.")
     }
@@ -38,7 +46,7 @@ async function processYouTubeVideo(
       return path.resolve(convertedFilePath)
     }
 
-    // Return the path of the downloaded (and possibly trimmed) audio
+    // Return the path of the downloaded (and possibly trimmed) video or audio file
     return downloadedFilePath
   } catch (error) {
     console.error("Error processing the YouTube video:", error)
