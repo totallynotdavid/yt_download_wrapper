bf152d3a3ae7fe626a7e7a90f61067fd23266532
David Duran <dadch1404@gmail.com>
2023-12-14 19:14:22 -0500

    Add production configuration file

---
+++ config/production.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /*
+    Options for audio quality: 'bestaudio' or 'ba' and 'worstaudio' or 'wa'
+  */
+  mediaAssetsFolder: 'media',
+  preferredDownloadQuality: 'bestaudio',
+  supportedFormats: {
+    'mp4': { codec: 'libx264', ext: 'mp4' },
+    'ogg': { codec: 'libopus', ext: 'ogg' },
+  },
+  defaultFormat: 'ogg'
+}
