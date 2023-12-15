15dd5bc588adbb22c4687c2a5fe31ce314b25fa0
David Duran <dadch1404@gmail.com>
2023-12-15 17:58:21 -0500

    Update exampleFunction calls in index.examples.js

--- examples/index.examples.js
+++ examples/index.examples.js
@@ -13,8 +13,10 @@ async function runExamples() {
   await exampleFunction("dQw4w9WgXcQ", 30, 120, "opus")
   await exampleFunction("XyAPmF8IeRQ", 10, 100, "ogg")
   await exampleFunction("el0N_MDcp0Y", 30, 120, "mp3")
-  await exampleFunction("xg43lcItgKk", 30, 120, "mp4")
-  await exampleFunction("k9tgLnI0fFc", null, null, (format = "mp4"))
+  await exampleFunction("xg43lcItgKk", 30, 120, "mp4") // Gets an mp4 file without video (bug?)
+  await exampleFunction("k9tgLnI0fFc", null, null, (format = "mp4")) // Gets the full video in best quality and mp4 format
+  await exampleFunction("4lJfA6ANgNM")              // Gets the full file in an opus format
+  await exampleFunction("BxqYUbNR-c0", 30, 120)     // Gets the full file in an opus format and trimmed
 }
 
 runExamples()
