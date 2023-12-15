e64a82a018297802b36a92364bf1972bf7bc49fa
David Duran <dadch1404@gmail.com>
2023-12-14 19:17:54 -0500

    Update media assets folder and preferred download quality in production config

--- config/production.config.js
+++ config/production.config.js
@@ -2,11 +2,11 @@ module.exports = {
   /*
     Options for audio quality: 'bestaudio' or 'ba' and 'worstaudio' or 'wa'
   */
-  mediaAssetsFolder: 'media',
-  preferredDownloadQuality: 'bestaudio',
+  mediaAssetsFolder: "media",
+  preferredDownloadQuality: "bestaudio",
   supportedFormats: {
-    'mp4': { codec: 'libx264', ext: 'mp4' },
-    'ogg': { codec: 'libopus', ext: 'ogg' },
+    mp4: { codec: "libx264", ext: "mp4" },
+    ogg: { codec: "libopus", ext: "ogg" },
   },
-  defaultFormat: 'ogg'
+  defaultFormat: "ogg",
 }
