1d45b23c96c214aab43b8a7c7067839405d56084
David Duran <dadch1404@gmail.com>
2023-12-15 01:43:20 -0500

    Update exampleFunction calls in index.examples.js

--- examples/index.examples.js
+++ examples/index.examples.js
@@ -15,10 +15,11 @@ async function exampleFunction(youtubeId, startTime, endTime, format) {
 }
 
 async function runExamples() {
-  await exampleFunction("dQw4w9WgXcQ", 30, 120, "ogg")
+  await exampleFunction("dQw4w9WgXcQ", 30, 120, "opus")
+  await exampleFunction("XyAPmF8IeRQ", 10, 100, "ogg")
   await exampleFunction("el0N_MDcp0Y", 30, 120, "mp3")
   await exampleFunction("xg43lcItgKk", 30, 120, "mp4")
-  await exampleFunction("k9tgLnI0fFc", null, null, (format = "webm"))
+  await exampleFunction("k9tgLnI0fFc", null, null, (format = "mp4"))
 }
 
 runExamples()
