07d7697ab79cabec3a4723545c4b5a7919c7c690
David Duran <dadch1404@gmail.com>
2023-12-14 19:19:06 -0500

    Refactor basic_example.js to use yt_downloader library

--- examples/basic_example.js
+++ examples/basic_example.js
@@ -1,3 +1,22 @@
-/*
-  This is a basic example of how to use the library.
-*/
+const yt_downloader = require("../src/index")
+
+async function run() {
+  try {
+    const youtubeId = "dQw4w9WgXcQ" // Replace with the desired YouTube video ID
+    const startTime = 30 // Optional: Start time in seconds
+    const endTime = 120 // Optional: End time in seconds
+    const format = "ogg" // Desired output format
+
+    const processedFilePath = await yt_downloader(
+      youtubeId,
+      startTime,
+      endTime,
+      format,
+    )
+    console.log("Processed video saved at:", processedFilePath)
+  } catch (error) {
+    console.error("An error occurred:", error)
+  }
+}
+
+run()
