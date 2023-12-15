1622699437b2c338573d89e1020625b4762c6a3f
David Duran <dadch1404@gmail.com>
2023-12-15 00:40:24 -0500

    Update production configuration with quality settings and audio formats

--- config/production.config.js
+++ config/production.config.js
@@ -1,12 +1,38 @@
 module.exports = {
-  /*
-    Options for audio quality: 'bestaudio' or 'ba' and 'worstaudio' or 'wa'
-  */
   mediaAssetsFolder: "media",
-  preferredDownloadQuality: "bestaudio",
-  supportedFormats: {
-    mp3: { codec: "libmp3lame", ext: "mp3" },
-    ogg: { codec: "libopus", ext: "ogg" },
-  },
+  defaultQuality: "best",
   defaultFormat: "ogg",
+
+  qualitySettings: {
+    best: {
+      audio: "bestaudio",
+      video: "bestvideo",
+    },
+    worst: {
+      audio: "worstaudio",
+      video: "worstvideo",
+    },
+  },
+
+  ytDlpAudioFormats: [
+    "best",
+    "aac",
+    "flac",
+    "mp3",
+    "m4a",
+    "opus",
+    "vorbis",
+    "wav",
+  ],
+
+  formats: {
+    mp4: { type: "video", ffmpegCodec: "libx264", ffmpegAudioCodec: "aac" },
+    webm: {
+      type: "video",
+      ffmpegCodec: "libvpx-vp9",
+      ffmpegAudioCodec: "libopus",
+    },
+    mp3: { type: "audio", ffmpegCodec: "libmp3lame" },
+    ogg: { type: "audio", ffmpegCodec: "libopus" },
+  },
 }
