866a7d1580515f58e3652759f9df11415fd90332
David Duran <dadch1404@gmail.com>
2023-12-14 19:18:04 -0500

    Refactor code to use consistent quotation marks and formatting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { downloadVideo } = require('./utils/ytdlpHelper');
-const { convertToFormat } = require('./utils/ffmpegHelper');
-const { ensureDirectoryExists } = require('./utils/folderStructure');
-const path = require('path');
-const config = require('../config/production.config');
+const { downloadVideo } = require("./utils/ytdlpHelper")
+const { convertToFormat } = require("./utils/ffmpegHelper")
+const { ensureDirectoryExists } = require("./utils/folderStructure")
+const path = require("path")
+const config = require("../config/production.config")
 
 /**
  * Processes a YouTube video by downloading it, optionally trimming, and converting to the desired format.
@@ -12,27 +12,36 @@ const config = require('../config/production.config');
  * @param {string} [format] - The desired output video format.
  * @returns {Promise<string>} - The path to the processed video file.
  */
-async function processYouTubeVideo(youtubeId, startTime, endTime, format = config.defaultFormat) {
+async function processYouTubeVideo(
+  youtubeId,
+  startTime,
+  endTime,
+  format = config.defaultFormat,
+) {
   try {
-    ensureDirectoryExists(path.resolve(config.mediaAssetsFolder));
+    ensureDirectoryExists(path.resolve(config.mediaAssetsFolder))
 
-    const downloadedFilePath = await downloadVideo(youtubeId, startTime, endTime);
+    const downloadedFilePath = await downloadVideo(
+      youtubeId,
+      startTime,
+      endTime,
+    )
     if (!downloadedFilePath) {
-      throw new Error('Failed to download the video or parse the file name.');
+      throw new Error("Failed to download the video or parse the file name.")
     }
-    const currentFormat = path.extname(downloadedFilePath).substring(1); // Extracts the extension without the dot
-    console.log('currentFormat', currentFormat)
+    const currentFormat = path.extname(downloadedFilePath).substring(1) // Extracts the extension without the dot
+    console.log("currentFormat", currentFormat)
 
     if (currentFormat !== format) {
-      return await convertToFormat(downloadedFilePath, format);
+      return await convertToFormat(downloadedFilePath, format)
     }
 
     // Return the path of the downloaded (and possibly trimmed) audio
-    return downloadedFilePath;
+    return downloadedFilePath
   } catch (error) {
-    console.error('Error processing the YouTube video:', error);
-    throw error;
+    console.error("Error processing the YouTube video:", error)
+    throw error
   }
 }
 
-module.exports = processYouTubeVideo;
+module.exports = processYouTubeVideo
