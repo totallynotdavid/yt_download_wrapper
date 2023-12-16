d410414f3d68ce96bc3cc64e49c09d7a692f7bcc
David Duran <dadch1404@gmail.com>
2023-12-16 01:16:02 -0500

    Refactor exampleFunction in index.examples.js

--- examples/index.examples.js
+++ examples/index.examples.js
@@ -1,6 +1,6 @@
 const ytDownloader = require("../src/index")
 
-async function exampleFunction(youtubeId, startTime, endTime, format) {
+async function exampleFunction({ youtubeId, startTime, endTime, format }) {
   try {
     const results = await ytDownloader(youtubeId, startTime, endTime, format)
     console.log(results)
@@ -10,13 +10,34 @@ async function exampleFunction(youtubeId, startTime, endTime, format) {
 }
 
 async function runExamples() {
-  await exampleFunction("dQw4w9WgXcQ", 30, 120, "opus")
-  await exampleFunction("XyAPmF8IeRQ", 10, 100, "ogg")
-  await exampleFunction("el0N_MDcp0Y", 30, 120, "mp3")
-  await exampleFunction("xg43lcItgKk", 30, 120, "mp4") // Gets an mp4 file without video (bug?)
-  await exampleFunction("k9tgLnI0fFc", null, null, (format = "mp4")) // Gets the full video in best quality and mp4 format
-  await exampleFunction("4lJfA6ANgNM")              // Gets the full file in an opus format
-  await exampleFunction("BxqYUbNR-c0", 30, 120)     // Gets the full file in an opus format and trimmed
+  await exampleFunction({
+    youtubeId: "dQw4w9WgXcQ",
+    startTime: 30,
+    endTime: 120,
+    format: "opus",
+  })
+
+  await exampleFunction({
+    youtubeId: "el0N_MDcp0Y",
+    startTime: 30,
+    endTime: 120,
+    format: "mp3",
+  })
+
+  // Gets the full file in an opus format
+  await exampleFunction("4lJfA6ANgNM")
+
+  // Gets the full file in an opus format and trimmed
+  await exampleFunction("BxqYUbNR-c0", 30, 120)
+
+  // Gets the full video in an mp4 format in the best quality
+  await exampleFunction({
+    youtubeId: "BxqYUbNR-c0",
+    format: "mp4",
+  })
+
+  // Currently, there is known bug when using this:
+  await exampleFunction("xg43lcItgKk", 30, 120, "mp4") // The result is an mp4 file without video, only audio
 }
 
 runExamples()
