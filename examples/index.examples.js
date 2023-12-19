0c5fbafa0eea1d685e9dd09692603fa739d75eee
David Duran <dadch1404@gmail.com>
2023-12-18 21:00:15 -0500

    docs: Add new examples for videoSize

--- examples/index.examples.js
+++ examples/index.examples.js
@@ -1,8 +1,22 @@
 const ytDownloader = require("../src/index")
 
-async function exampleFunction({ youtubeId, startTime, endTime, format }) {
+async function exampleFunction({
+  youtubeId,
+  startTime,
+  endTime,
+  format,
+  quality,
+  videoSize,
+}) {
   try {
-    const results = await ytDownloader(youtubeId, startTime, endTime, format)
+    const results = await ytDownloader(
+      youtubeId,
+      startTime,
+      endTime,
+      format,
+      quality,
+      videoSize,
+    )
     console.log(results)
   } catch (error) {
     console.error(`An error occurred processing [${youtubeId}]:`, error)
@@ -10,6 +24,7 @@ async function exampleFunction({ youtubeId, startTime, endTime, format }) {
 }
 
 async function runExamples() {
+  console.log("We are expecting an opus file with a duration of 90 seconds")
   await exampleFunction({
     youtubeId: "dQw4w9WgXcQ",
     startTime: 30,
@@ -17,6 +32,7 @@ async function runExamples() {
     format: "opus",
   })
 
+  console.log("We are expecting an mp3 file with a duration of 90 seconds")
   await exampleFunction({
     youtubeId: "el0N_MDcp0Y",
     startTime: 30,
@@ -24,31 +40,43 @@ async function runExamples() {
     format: "mp3",
   })
 
-  // Gets the full file in an opus format
+  console.log(
+    "We are expecting an opus file (complete video) in the best quality",
+  )
   await exampleFunction({
     youtubeId: "4lJfA6ANgNM",
   })
 
-  // Gets the full file in an opus format and trimmed
+  console.log("We are expecting an opus file with a duration of 90 seconds")
   await exampleFunction({
     youtubeId: "BxqYUbNR-c0",
     startTime: 30,
     endTime: 120,
   })
 
-  // Gets the full video in an mp4 format in the best quality
+  console.log(
+    "We are expecting an mp4 file (complete video) in the best quality",
+  )
   await exampleFunction({
     youtubeId: "BxqYUbNR-c0",
     format: "mp4",
   })
 
-  // Currently, there is known bug when using this:
+  // Currently, there is known bug when using this: mp4 file without video, only audio
+  console.log("We are expecting an mp4 file with a duration of 90 seconds")
   await exampleFunction({
     youtubeId: "xg43lcItgKk",
     startTime: 30,
     endTime: 120,
     format: "mp4",
-  }) // The result is an mp4 file without video, only audio
+  })
+
+  console.log("We are expecting an mp4 file with a size of no more than 14MB")
+  await exampleFunction({
+    youtubeId: "JC6budcACNE",
+    format: "mp4",
+    videoSize: "small",
+  })
 }
 
 runExamples()
